4f63554ddb37703d2b25aafef50d5ca39a181328
ogane0112 <kai513272@gmail.com>
2024-09-12 18:00:24 +0900

    next_public消した

--- app/api/result/route.js
+++ app/api/result/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
     // 順位入力があるものをフィルタリング
-    const rep = await fetch(process.env.NEXT_PUBLIC_API_URL);
+    const rep = await fetch(process.env.API_URL);
     const data = await rep.json();
     const resultData = data.filter(runner => runner.順位入力 !== "");
     
--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import Combine from "./components/Combine"
 
 export default async function Page() {
-  const rep = await fetch(process.env.NEXT_PUBLIC_API_URL_COMBINE);
+  const rep = await fetch(process.env.API_URL_COMBINE);
   const data = await rep.json();
 
   return (
