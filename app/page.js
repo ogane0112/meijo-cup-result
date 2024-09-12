c605d3b411628f6a8bafaff93308fa176b3802f9
ogane0112 <kai513272@gmail.com>
2024-09-12 18:27:38 +0900

    a

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
 import Combine from "./components/Combine"
-
+import { headers } from 'next/headers';
 export default async function Page() {
-  const rep = await fetch(process.env.API_URL_COMBINE);
+  const headersList = headers();
+  const host = headersList.get('host');
+  const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http';
+  const rep = await fetch(`${protocol}://${host}${process.env.API_URL_COMBINE}`);
   const data = await rep.json();
 
   return (
--- app/result/page.jsx
+++ app/result/page.jsx
@@ -1,7 +1,10 @@
 import Result from "../components/Result";
-
+import { headers } from 'next/headers';
 export default async function Page(){
-    const rep = await fetch(process.env.API_URL_RESULT);
+    const headersList = headers();
+    const host = headersList.get('host');
+    const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http';
+    const rep = await fetch(`${protocol}://${host}${process.env.API_URL_RESULT}`);
     const data = await rep.json();
     return(
         <main>
